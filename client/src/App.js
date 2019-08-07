2fa06609b4c8e9e05cdc7f9fa4be21a21a0e2bca
Steven Espinal <stevenjesusespinal@gmail.com>
2019-08-07 14:14:29 -0400

    Create messages displayed to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,91 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, {Component} from "react";
+import {Socket} from 'react-socket-io';
+import axios from "axios";
 import "./App.css";
 
+const uri = "http://localhost:8080";
+const options = {transports: ["websocket"]};
+
 class App extends Component {
+
+  state = {
+    userName: "",
+    message: "",
+    nameSubmitted: false,
+    messages: []
+  };
+
+  componentDidMount() {
+    axios.get("/messages").then(response => {
+      this.setState({
+        messages: response.data
+      })
+    })
+  }
+
+
+  refresh = () => {
+    axios.get("/messages").then(response => {
+      this.setState({
+        messages: response.data
+      })
+    })
+  };
+
+  handleChange = (e) => {
+    const {name, value} = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  updateSubmit = () => {
+    this.setState({
+      nameSubmitted: true
+    })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("/messages", {"name": this.state.userName, "message": this.state.message}).then(response => {
+      console.log(response);
+      this.setState({
+        messages: response.data
+      });
+      console.log(this.state.messages);
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Socket uri={uri} options={options}>
+        <div className="App">
+          {this.state.nameSubmitted ? (<div id="entrance">
+            <ul id="messages">
+              {this.state.messages.map((msg, i) => {
+                return (
+                  <div style={{borderBottom: "5px solid forrestgreen"}} key={i}>
+                    <h3>{msg.name}</h3>
+                    <span>{msg.message}</span>
+                  </div>
+                );
+              })}
+            </ul>
+            <button className="btn btn-info" onClick={this.refresh}>Refresh</button>
+            <form action="/messages" method="POST" id="chatForm">
+              <input id="userName" name="userName" value={this.state.userName} onChange={this.handleChange} disabled/>
+              <input name="message" value={this.state.message} onChange={this.handleChange} id="txt"
+                     placeholder="type your message here..."/>
+              <button onClick={this.handleSubmit}>Send</button>
+            </form>
+
+          </div>) : (<div id="user" className="offset-md-5">
+            <input onChange={this.handleChange} name="userName" value={this.state.userName} type="text"
+                   placeholder="Enter a username" id="userName"/>
+            <button id="enter" className="btn btn-success" onClick={this.updateSubmit}>Enter</button>
+          </div>)}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Socket>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,37 +1,55 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+var server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const axios = require("axios");
+
+// const socket = new WebSocket('ws:localhost:8080');
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+var initialMessage = [{
+  name: "Bot",
+  message: "Please type below to talk to your friends in the chat"
+}];
+
 // Define API routes here
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.send("hello world");
 });
-io.sockets.on("connection", function(socket) {
-  socket.on("username", function(username) {
+
+app.get("/messages", (req, res) => {
+  res.json(initialMessage);
+});
+
+app.post("/messages", (req, res) => {
+  console.log(req.body);
+  initialMessage.push(req.body);
+  res.json(initialMessage);
+  res.status(200).end();
+
+});
+io.sockets.on("connection", function (socket) {
+  socket.on("username", function (username) {
     socket.username = username;
     io.emit("is_online", '🔵 <i>' + socket.username + ' join the chat..</i>');
   });
 
-  socket.on("disconnect", function(username) {
+  socket.on("disconnect", function (username) {
     io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
   });
 
-  socket.on('chat_message', function(message) {
+  socket.on('chat_message', function (message) {
     io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
   });
-})
+});
 
-const server = http.listen(8080, function() {
+server.listen(8080, function () {
   console.log("listening on port 8080");
 });
