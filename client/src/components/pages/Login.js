3bb85dc418e27140b64828d8c707592f3b82a153
sajad1993gh <sajad1993gh@gmail.com>
2019-08-08 15:32:19 -0400

    styling login signup

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -30,11 +30,11 @@ class Login extends Component {
             <div>
                 {/* <Navbar /> */}
                 <div className='container'>
-                    <div className="form login-form">
+                    <div className="form login-form" style={{"width": "400px" , "margin": "50px auto"}}>
                         <input type="text" name="username" placeholder="username" />
                         <input type="text" name="password" placeholder="password" />
-                            <div className="buttons">
-                                <button className="waves-effect waves-light btn login">Login</button>
+                            <div className="buttons" style={{"marginLeft" : "20%", "marginTop": "10px"}}>
+                                <button className="waves-effect waves-light btn login" style={{"width": "45%","margin": "2px"}}>Login</button>
                                 <a className="waves-effect waves-light btn signup" href="/signup">Signup</a>
                             </div>
                     </div>
