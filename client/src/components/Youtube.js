7d1d020d705981b2452b746885604af3863cf49c
sajad1993gh <sajad1993gh@gmail.com>
2019-08-09 11:51:29 -0400

    Youtube is up

--- client/src/components/Youtube.js
+++ client/src/components/Youtube.js
@@ -7,15 +7,7 @@ class Youtube extends Component {
     state = {
       }
 
-    // var player;
-    // function onYouTubeIframeAPIReady() {
-    //     player = new YT.Player('existing-iframe-example', {
-    //         events: {
-    //           'onReady': onPlayerReady,
-    //           'onStateChange': onPlayerStateChange
-    //         }
-    //     });
-    //   }
+
 
     render() {
 
@@ -23,15 +15,17 @@ class Youtube extends Component {
 
         <div >
 
-        <input type="Video" style={{"maxWidth": "300px", "marginRight": ".5em", "marginBottom": ".5em", "float": "left"}} className="form-control" id="inputVideoId" placeholder="Video ID / URL" />
+        <input type="Video" style={{"maxWidth": "300px", "marginRight": ".5em", "marginBottom": ".5em"}} className="form-control" id="inputVideoId" placeholder="Enter Video ID / URL" />
         <iframe id="existing-iframe-example"
                 width="640" height="360"
                 src="https://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1"
-                frameborder="1"
+                frameborder="0.7"
                 style={{"border": "solid 4px #37474F"}}
         ></iframe>
 
-
+        <script id='iframe-demo' src='https://www.youtube.com/iframe_api' >
+        </script>
+        <br></br>
         <button id="playButton" onClick="playVideo(roomnum)" style={{"marginTop": "0.5em"}} className="btn btn-primary"><i className="fa fa-play"></i> Play / <i className="fa fa-pause"></i> Pause</button>
 
         <button id="syncbutton" onClick="syncVideo(roomnum);syncAlert()" style={{"marginTop": "0.5em"}} className="btn btn-primary"><i className="fa fa-sync"></i> Sync</button>
