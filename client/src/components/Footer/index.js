c4ba555f7926a8aab970d8ba643c0cb9fdd2d9e1
sajad1993gh <sajad1993gh@gmail.com>
2019-08-12 14:47:58 -0400

    footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,9 +2,30 @@ import React from "react";
 import "./style.css";
 
 
-function Footer({children}) {
+function Footer() {
   return (
-    <footer className="page-footer font-small indigo"> {children}</footer>
+    <footer class="page-footer" style={{"background": "rgb(2, 17, 49)",
+    "color": "rgb(235, 235, 235)", "height": "auto"}}>
+        <div class="container">
+            <div class="row">
+                <div class="col l4  s6">
+                    <h6 class="white-text">Developers <i class="fab fa-github" style={{"font-size": "20px", "color":"#ffffff"}}></i> </h6>
+                    <span style={{"font-size": "13px"}}>
+                        <a class="grey-text text-lighten-3" href="" target="_blank">team member 1 | </a> 
+                        <a class="grey-text text-lighten-3" href="" target="_blank">member 2 | </a>  
+                        <a class="grey-text text-lighten-3" href="" target="_blank">member 3 | </a>
+                        <a class="grey-text text-lighten-3" href="" target="_blank">member 4 | </a>
+                        <a class="grey-text text-lighten-3" href="" target="_blank">member 5</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © 2019 APP NAME
+            </div>
+        </div>
+    </footer>
   );
 }
 
