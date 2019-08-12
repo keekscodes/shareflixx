9255372e4759657ed9033183baa5708597fa25b0
Steven Espinal <stevenjesusespinal@gmail.com>
2019-08-12 16:02:10 -0400

    latest files

--- client/src/components/pages/LoginForm.js
+++ client/src/components/pages/LoginForm.js
@@ -1,19 +1,25 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 
-class LoginForm extends Component {
+class SignupForm extends Component {
   render() {
     return (
       <div className='container'>
-        <div className="form login-form" style={{"width": "400px", "margin": "50px auto"}}>
-          <input type="text" name="username" placeholder="username" value={this.props.username}
-                 onChange={this.props.handleInputChange}/>
-          <input type="text" name="password" placeholder="password" value={this.props.password}
-                 onChange={this.props.handleInputChange}/>
+        <div className="form signup-form" style={{"width": "400px", "margin": "50px auto"}}>
+          <input type="text" name="firstName" placeholder="First Name"
+                 onChange={this.props.handleInputChange} value={this.props.firstName}/>
+          <input type="text" name="lastName" placeholder="lastName"
+                 onChange={this.props.handleInputChange} value={this.props.lastName}/>
+          <input type="text" name="username" placeholder="username"
+                 onChange={this.props.handleInputChange} value={this.props.username}/>
+          <input type="text" name="email" placeholder="email"
+                 onChange={this.props.handleInputChange} value={this.props.email}/>
+          <input type="password" name="password" placeholder="password"
+                 onChange={this.props.handleInputChange} value={this.props.password}/>
           <div className="buttons" style={{"marginLeft": "20%", "marginTop": "10px"}}>
-            <button className="waves-effect waves-light btn login" style={{"width": "45%", "margin": "2px"}}
-                    onClick={this.props.handleFormSubmit}>Login
+            <button className="waves-effect waves-light btn signup" onClick={this.props.handleFormSubmit}
+                    style={{"width": "45%", "margin": "2px"}}>Signup
             </button>
-            <a className="waves-effect waves-light btn signup" href="/signup">Signup</a>
+            <a className="waves-effect waves-light btn login" href="/login">Login</a>
           </div>
         </div>
       </div>
@@ -21,4 +27,4 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+export default SignupForm;
