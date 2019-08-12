17524a52db33885ab7dc2a36e74dff01bcd09d86
Steven Espinal <43868709+stevenespinal@users.noreply.github.com>
2019-08-12 13:14:36 -0400

    Revert "Invite btn"

--- client/src/components/pages/Show.js
+++ client/src/components/pages/Show.js
@@ -8,33 +8,28 @@ import Footer from "../Footer";
 import Card from "../Card";
 import Chat from "../Chat";
 import Youtube from "../Youtube";
-import { Col, Row, Container } from "../Grid";
-import Modal from "../Modal";
-import Logo from '../Logo/logo.jpg';
-
+import {Col, Row, Container} from "../Grid";
 
 class Show extends Component {
   render() {
     return (
       <div>
 
-        <Col size="md-3">
-            <button style={{"backgroundColor": "#06088d"}} type="button" className="nonmobile-hide btn btn-info btn-sm invite-button" data-toggle="modal" data-target="#inviteModal"><i style={{"width": "30px", "height": "30px"}} className="fas fa-user-plus"></i><h5>Invite</h5></button>
-            <Modal></Modal>
-            <a class="navbar-brand js-scroll-trigger" href="/show"><img src={Logo} alt="App logo" height="40px" width="100px" /></a>
-        </Col>
+        <Container fluid>
+          <Row>
+            <Col size="md-6">
+              <Title>App Name</Title>
+            </Col>
 
-         <Col size="md-3">
-         <Container>
-            <Button>Home</Button>
-            <Button>Logout</Button>
+            <Col size="md-6">
+              <Container>
+                <Button>Home</Button>
+                <Button>Logout</Button>
+              </Container>
+            </Col>
+
+          </Row>
         </Container>
-        </Col>
-
-
-
-                </Row>
-                </Container>
 
         <Container fluid>
           <Row>
@@ -61,11 +56,13 @@ class Show extends Component {
           </Row>
         </Container>
 
-          <Container fluid>
-            <Row>
-              <Col size="md-6">
-                <Jumbotron>
-
+        <Container fluid>
+          <Row>
+            <Col size="md-6">
+              <Jumbotron>
+                {/* <Embed>
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/lXMskKTw3Bc" title="The best thing you'll ever see"></iframe>
+                    </Embed> */}
                 <Youtube></Youtube>
                 {/* <h1>What Books Should I Read?</h1> */}
               </Jumbotron>
@@ -84,20 +81,25 @@ class Show extends Component {
                                 <p class="card-text">Hello.</p>
                             </div>
                         </Card> */}
-                        </Col>
-                        <Col size="md-6">
-                        <Card>
-                        <div className="card-body">
-                                <h5 className="card-title">Users</h5>
-                                <ul>
-                                    <li>Me</li>
-                                </ul>
-                            </div>
-                        </Card>
-                        </Col>
-                    </Row>
-                    <Row>
-
+                  </Col>
+                  <Col size="md-6">
+                    <Card>
+                      <div class="card-body">
+                        <h5 class="card-title">Users</h5>
+                        <ul>
+                          <li>Me</li>
+                        </ul>
+                      </div>
+                    </Card>
+                  </Col>
+                </Row>
+                <Row>
+                  {/* <Container fluid
+                        style={{
+                           backgroundColor:"white"
+                        }}>
+                        <Chat></Chat>
+                        </Container> */}
                 </Row>
               </Jumbotron>
             </Col>
