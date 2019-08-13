4bf4583ca5b83e24dbb9e92f2f63c9033ce88f4f
Steven Espinal <stevenjesusespinal@gmail.com>
2019-08-13 04:50:59 -0400

    restructured login pages into separate components. reformatted code as well

--- client/src/components/pages/LoginForm.js
+++ client/src/components/pages/LoginForm.js
@@ -1,30 +1,22 @@
-import React, {Component} from "react";
+import React from "react";
 
-class SignupForm extends Component {
-  render() {
-    return (
-      <div className='container'>
-        <div className="form signup-form" style={{"width": "400px", "margin": "50px auto"}}>
-          <input type="text" name="firstName" placeholder="First Name"
-                 onChange={this.props.handleInputChange} value={this.props.firstName}/>
-          <input type="text" name="lastName" placeholder="lastName"
-                 onChange={this.props.handleInputChange} value={this.props.lastName}/>
-          <input type="text" name="username" placeholder="username"
-                 onChange={this.props.handleInputChange} value={this.props.username}/>
-          <input type="text" name="email" placeholder="email"
-                 onChange={this.props.handleInputChange} value={this.props.email}/>
-          <input type="password" name="password" placeholder="password"
-                 onChange={this.props.handleInputChange} value={this.props.password}/>
-          <div className="buttons" style={{"marginLeft": "20%", "marginTop": "10px"}}>
-            <button className="waves-effect waves-light btn signup" onClick={this.props.handleFormSubmit}
-                    style={{"width": "45%", "margin": "2px"}}>Signup
-            </button>
-            <a className="waves-effect waves-light btn login" href="/login">Login</a>
-          </div>
-        </div>
+function LoginForm(props) {
+  return (
+    <form className={props.active ? "form-signin" : "form-signup-left"} action="" method="post" name="form">
+      <label htmlFor="username">Username</label>
+      <input className="form-styling" type="text" name="username" placeholder="" value={props.username}
+             onChange={props.handleInputChange}/>
+      <label htmlFor="password">Password</label>
+      <input className="form-styling" type="password" name="password" placeholder="" value={props.password}
+             onChange={props.handleInputChange}/>
+      <input type="checkbox" id="checkbox"/>
+      <label htmlFor="checkbox"><span className="ui"></span>Keep me signed in</label>
+      <div className="btn-animate">
+        <a className="btn-signin" onClick={props.loginSubmit}>Sign in</a>
       </div>
-    );
-  }
+      <a href="#">Forgot your password?</a>
+    </form>
+  );
 }
 
-export default SignupForm;
+export default LoginForm;
--- client/src/components/pages/SignupForm.js
+++ client/src/components/pages/SignupForm.js
@@ -1,30 +1,21 @@
-import React, {Component} from "react";
+import React from "react";
 
-class SignupForm extends Component {
-  render() {
+function SignupForm (props) {
     return (
-      <div className='container'>
-        <div className="form signup-form" style={{"width": "400px", "margin": "50px auto"}}>
-          <input type="text" name="firstName" placeholder="First Name"
-                 onChange={this.props.handleInputChange} value={this.props.firstName}/>
-          <input type="text" name="lastName" placeholder="lastName"
-                 onChange={this.props.handleInputChange} value={this.props.lastName}/>
-          <input type="text" name="username" placeholder="username"
-                 onChange={this.props.handleInputChange} value={this.props.username}/>
-          <input type="text" name="email" placeholder="email"
-                 onChange={this.props.handleInputChange} value={this.props.email}/>
-          <input type="password" name="password" placeholder="password"
-                 onChange={this.props.handleInputChange} value={this.props.password}/>
-          <div className="buttons" style={{"marginLeft": "20%", "marginTop": "10px"}}>
-            <button className="waves-effect waves-light btn signup" onClick={this.props.handleFormSubmit}
-                    style={{"width": "45%", "margin": "2px"}}>Signup
-            </button>
-            <a className="waves-effect waves-light btn login" href="/login">Login</a>
-          </div>
-        </div>
-      </div>
+      <form className={props.active ? "form-signup": "form-signin-down"} action="" method="post" name="form">
+        <label htmlFor="fullname">First Name</label>
+        <input className="form-styling" type="text" name="firstName" placeholder="" value={props.firstName} onChange={props.handleInputChange}/>
+        <label htmlFor="fullname">Last Name</label>
+        <input className="form-styling" type="text" name="lastName" placeholder="" value={props.lastName} onChange={props.handleInputChange}/>
+        <label htmlFor="email">Email</label>
+        <input className="form-styling" type="text" name="email" placeholder="" value={props.email} onChange={props.handleInputChange}/>
+        <label htmlFor="username">Create Username</label>
+        <input className="form-styling" type="text" name="username" placeholder="" value={props.username} onChange={props.handleInputChange}/>
+        <label htmlFor="password">Password</label>
+        <input className="form-styling" type="text" name="password" placeholder="" value={props.password} onChange={props.handleInputChange}/>
+          <a className="btn-signup" onClick={props.signupSubmit}>Sign Up</a>
+      </form>
     );
-  }
 }
 
 export default SignupForm;
