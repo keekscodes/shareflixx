8491b9a7e2f29d5771ddfe6fd333b8524f7c2723
Steven Espinal <stevenjesusespinal@gmail.com>
2019-08-13 15:44:07 -0400

    login progressing

--- config/passport.js
+++ config/passport.js
@@ -5,13 +5,14 @@ const LocalStrategy = require('passport-local');
 const Users = mongoose.model('Users');
 
 passport.use(new LocalStrategy({
-  usernameField: 'user[email]',
+  usernameField: 'user[username]',
   passwordField: 'user[password]',
-}, (email, password, done) => {
-  Users.findOne({email})
+}, (username, password, done) => {
+  // console.log('EMAIL-------->', email);
+  Users.findOne({username})
     .then((user) => {
       if (!user || !user.validatePassword(password)) {
-        return done(null, false, {errors: {'email or password': 'is invalid'}});
+        return done(null, false, {errors: {'username or password': 'is invalid'}});
       }
 
       return done(null, user);
