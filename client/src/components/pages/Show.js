9343ae7514a4c8291b9421fbfe3a89a4eb024f1e
Steven Espinal <stevenjesusespinal@gmail.com>
2019-08-14 09:44:53 -0400

    latest files

--- client/src/components/pages/Show.js
+++ client/src/components/pages/Show.js
@@ -1,10 +1,5 @@
 import React, {Component} from "react";
 import Jumbotron from "../Jumbotron";
-import Navigation from "../Navigation";
-// import Title from "../Title";
-// import Button from "../Button";
-import Footer from "../Footer";
-// import Card from "../Card";
 import Chat from "../Chat";
 import Youtube from "../Youtube";
 import {Col, Row, Container} from "../Grid";
@@ -22,7 +17,6 @@ class Show extends Component {
   logOut = () => {
     window.location.reload();
     return window.location.href = "/login";
-    // this.reloadPage();
   };
   reloadPage = () => {
     setTimeout(() => {
@@ -44,13 +38,8 @@ class Show extends Component {
 
   render() {
     return (
-      <div>
-        <div className="mb-5">
-          <Navigation/>
-        </div>
+      <div className="mt-5">
         <Container fluid>
-
-          {/* <Row> */}
           <Col size="md-6">
             <h5
               style={{
@@ -68,20 +57,8 @@ class Show extends Component {
                 borderTopLeftRadius: "4em",
 
                 borderTopRightRadius: "4em",
-                // backgroundImage:`url(${"http://images4.fanpop.com/image/photos/23100000/Green-leaf-close-up-green-23162757-2560-1920.jpg"})`,
-                // backgroundSize: "100px"
-
               }}>Group Name</h5>
-
-            {/* </Container> */}
-
-            {/* }}>Group Name</h5>
-
-                  borderTopRightRadius: "4em",
-                }}>Group Name</h5> */}
-
           </Col>
-          {/* </Row> */}
         </Container>
 
         <Container fluid>
@@ -109,7 +86,6 @@ class Show extends Component {
             </Col>
           </Row>
         </Container>
-        <Footer/>
       </div>
     );
   }
