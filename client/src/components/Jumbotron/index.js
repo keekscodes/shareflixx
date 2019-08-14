d7bd5858fbd280d197d55b603fb36520456d760c
Kimberly <kamueller2@gmail.com>
2019-08-14 11:42:07 -0400

    jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -10,7 +10,13 @@ function Jumbotron({children}) {
         marginLeft: "0em",
         //  marginTop:"2%",
         textAlign: "center",
-        backgroundColor: "steelblue"
+        // backgroundColor: "steelblue"
+        // backgroundColor: "#dc3545"
+        // background: "-webkit-linear-gradient(top, #a90329 0%, #8f0222 44%, #6d0019 100%)", /* Chrome10-25,Safari5.1-6 */
+            background: "linear-gradient(to bottom, #a90329 0%, #8f0222 44%, #6d0019 100%", /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+            // filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr='#a90329'", 
+            // endColorstr='#6d0019', 
+            // GradientType=0 /* IE6-9 */
       }}
       className="jumbotron"
     >
