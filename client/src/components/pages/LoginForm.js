c4f436d90c5d2f9dbe24b66548602b0d2c007631
Steven Espinal <stevenjesusespinal@gmail.com>
2019-08-27 11:26:43 -0400

    destructured props and restructured LoginForm back to a functional component

--- client/src/components/pages/LoginForm.js
+++ client/src/components/pages/LoginForm.js
@@ -1,27 +1,25 @@
-import React, {Component} from "react";
+import React from "react";
 import {Link} from "react-router-dom";
 
-class LoginForm extends Component {
-  render() {
+function LoginForm (props) {
+  const {active, username, handleInputChange, password, loginSubmit, spinner} = props;
     return (
-      <form className={this.props.active ? "form-signin" : "form-signup-left"} action="" method="post" name="form">
+      <form className={active ? "form-signin" : "form-signup-left"} action="" method="post" name="form">
         <label htmlFor="username">Username</label>
-        <input className="form-styling" type="text" name="username" placeholder="" value={this.props.username}
-               onChange={this.props.handleInputChange}/>
+        <input className="form-styling" type="text" name="username" placeholder="" value={username}
+               onChange={handleInputChange}/>
         <label htmlFor="password">Password</label>
-        <input className="form-styling" type="password" name="password" placeholder="" value={this.props.password}
-               onChange={this.props.handleInputChange}/>
+        <input className="form-styling" type="password" name="password" placeholder="" value={password}
+               onChange={handleInputChange}/>
         <input type="checkbox" id="checkbox"/>
         <label htmlFor="checkbox"><span className="ui"/>Keep me signed in</label>
         <div className="btn-animate">
-          {/* <Link to="/show" className="btn-signin btn-danger" onClick={props.loginSubmit}>{props.loggedIn ? <i class="fas fa-spinner fa-spin"/> : "Sign In"}</Link> */}
-          <Link to="/show" className="btn-signin btn-danger" onClick={this.props.loginSubmit}>{this.props.spinner ? <i className="fas fa-spinner fa-spin"/> : "Sign In"}</Link>
+          {/* <Link to="/show" className="btn-signin btn-danger" onClick={loginSubmit}>{loggedIn ? <i class="fas fa-spinner fa-spin"/> : "Sign In"}</Link> */}
+          <Link to="/show" className="btn-signin btn-danger" onClick={loginSubmit}>{spinner ? <i className="fas fa-spinner fa-spin"/> : "Sign In"}</Link>
         </div>
         <a href="/login" className="pw">Forgot your password?</a>
       </form>
     );
-
-  }
 }
 
 export default LoginForm;
