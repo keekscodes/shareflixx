b9050375dfb40bebd89f02c48d59afde2ebc19ac
Steven Espinal <stevenjesusespinal@gmail.com>
2019-09-04 10:07:48 -0400

    updated account username in dropdown

--- client/src/components/pages/Show.js
+++ client/src/components/pages/Show.js
@@ -2,16 +2,34 @@ import React, {Component} from "react";
 import Chat from "../Chat";
 import Youtube from "../Youtube";
 import Navigation from "../Navigation/index"
+import axios from "axios";
 
 class Show extends Component {
   state = {
-    toggle: false
+    toggle: false,
+    user: []
   };
 
   componentDidMount() {
-    console.log(this.props.location.state.username);
+    // console.log(this.props.location.state.username);
     this.isTokenExpired() ? (this.logOut()) : (this.reloadPage());
+    this.getToken()
   }
+
+  getToken = () => {
+    let token = sessionStorage.getItem("token");
+
+    axios.get("/api/users/current", {
+      headers: {
+        'Content-Type': "application/json",
+        'Authorization': "Token " + token
+      }
+    }).then(res => {
+      this.setState({
+        user: res.data.user
+      })
+    })
+  };
 
   logOut = () => {
     window.location.reload();
@@ -39,13 +57,13 @@ class Show extends Component {
     return (
       <div id="show-route" className="container-fluid">
         {/*<Navigation username={this.props.location.state.username}/>*/}
-        <Navigation>{this.props.location.state.username}</Navigation>
+        <Navigation>{this.state.user.username}</Navigation>
         <div className="row">
           <div className="col-md-9">
             <Youtube/>
           </div>
           <div id="message-form" className="col-md-3">
-            <Chat username={this.props.location.state.username}/>
+            <Chat username={this.state.user.username}/>
           </div>
         </div>
       </div>
