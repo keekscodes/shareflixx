1cbd846090076621c8627c4e6fba7ab8e358f967
Kimberly <kamueller2@gmail.com>
2019-09-06 17:12:04 -0400

    revising build

--- app.js
+++ app.js
@@ -31,13 +31,11 @@ if (!isProduction) {
   app.use(errorHandler());
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (isProduction) {
 	app.use(express.static('client/build'));
 }
 
-app.get('/*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+
 //Configure Mongoose
 // mongoose.connect('mongodb://user:sharefliXX2019@ds219078.mlab.com:19078/heroku_ck5c5l5m', {useNewUrlParser: true});
 mongoose.connect('mongodb://localhost/shareflix', {useNewUrlParser: true});
